9876ecde981442ee196a5d84177f5e482dc69dfe
Luke Ellul <ellul.luke@gmail.com>
2022-11-19 15:41:59 +0000

    added Apollo GraphQL

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { InMemoryCache } from '@apollo/client/core';
+import { APOLLO_OPTIONS } from 'apollo-angular';
+import { HttpLink } from 'apollo-angular/http';
 import { AppComponent } from './app.component';
 
 @NgModule({
     declarations: [AppComponent],
     imports: [BrowserModule],
-    providers: [],
+    providers: [
+        {
+            provide: APOLLO_OPTIONS,
+            useFactory: (httpLink: HttpLink) => {
+                return {
+                    cache: new InMemoryCache(),
+                    link: httpLink.create({
+                        uri: 'https://api-staging.csgoroll.com/graphql',
+                        withCredentials: true
+                    })
+                };
+            }
+        }
+    ],
     bootstrap: [AppComponent]
 })
 export class AppModule {}
