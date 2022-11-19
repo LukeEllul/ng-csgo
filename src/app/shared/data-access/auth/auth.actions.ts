da44a3fb5266c38994e3c50a10102848772c0efb
Luke Ellul <ellul.luke@gmail.com>
2022-11-19 20:23:34 +0000

    created user actions

--- src/app/shared/data-access/auth/auth.actions.ts
+++ src/app/shared/data-access/auth/auth.actions.ts
@@ -1,5 +1,12 @@
-import { createAction } from '@ngrx/store';
+import { createAction, props } from '@ngrx/store';
+import { User } from '../models/user.model';
+import { Wallet } from '../models/wallet.model';
 
 export const csgoLogin = createAction('[Auth API] CSGO Login');
 
 export const csgoLoginSuccess = createAction('[Auth API] CSGO Login Success');
+
+export const fetchedUserSuccess = createAction(
+    '[Auth API] Fetched User Success',
+    props<{ user: User; wallets: Wallet[] }>
+);
--- src/app/shared/data-access/auth/auth.reducer.ts
+++ src/app/shared/data-access/auth/auth.reducer.ts
@@ -1,19 +1,25 @@
 import { createFeature, createReducer, on } from '@ngrx/store';
+import { User } from '../models/user.model';
 import * as AuthActions from './auth.actions';
 
 export interface State {
     loggedIn: boolean;
+    user: User | null;
 }
 
 export const initialState: State = {
-    loggedIn: false
+    loggedIn: false,
+    user: null
 };
 
 const authFeature = createFeature({
     name: 'auth',
     reducer: createReducer(
         initialState,
-        on(AuthActions.csgoLoginSuccess, () => ({ loggedIn: true }))
+        on(AuthActions.csgoLoginSuccess, (state) => ({
+            ...state,
+            loggedIn: true
+        }))
     )
 });
 
