ecaf78e07b6309823281f2372815f40bda68a588
Luke Ellul <ellul.luke@gmail.com>
2022-11-20 08:19:00 +0000

    created view-box component

--- src/app/view-box/feature/view-box.component.ts
+++ src/app/view-box/feature/view-box.component.ts
@@ -1,13 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import * as viewBox from '../data-access/view-box.reducer';
-import * as ViewBoxActions from '../data-access/view-box.actions';
+import * as viewBox from '../../data-access/view-box.reducer';
+import * as ViewBoxActions from '../../data-access/view-box.actions';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
     selector: 'app-view-box',
     templateUrl: './view-box.component.html',
-    styleUrls: ['./view-box.component.scss']
+    styleUrls: ['./view-box.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ViewBoxComponent implements OnInit {
     viewBox$ = this.store.select(viewBox.selectViewBox);
