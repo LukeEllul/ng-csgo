12c0e13ce96db91abe5ed1fd0a49a08931729693
Luke Ellul <ellul.luke@gmail.com>
2022-11-20 03:14:51 +0000

    added loading functionality to router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { combineLatest } from 'rxjs';
 import * as AuthActions from './shared/data-access/auth/auth.actions';
+import * as auth from './shared/data-access/auth/auth.reducer';
 
 @Component({
     selector: 'app-root',
@@ -9,6 +11,14 @@ import * as AuthActions from './shared/data-access/auth/auth.actions';
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent implements OnInit {
+    private user$ = this.store.select(auth.selectUser);
+    private userLoading$ = this.store.select(auth.selectLoading);
+
+    vm$ = combineLatest({
+        user: this.user$,
+        userLoading: this.userLoading$
+    });
+
     constructor(private store: Store) {}
 
     ngOnInit(): void {
